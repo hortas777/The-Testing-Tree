03a8f1d4fe1400eba75b7f38ab83581bc9ecf353
hortas777 <eduard0madeira2007@gmail.com>
2024-01-19 15:28:13 +0000

    asd
    asd

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("d", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#000000",
+    color: "#0f522d",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "duplis", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -26,3 +26,33 @@ addLayer("d", {
     ],
     layerShown(){return true}
 })
+
+addLayer("t", {
+    name: "tripli", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#000000",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
+    resource: "triplis", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "d", description: "D: reset for duplis", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
+
